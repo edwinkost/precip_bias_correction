bafa53a1a44735c9791bb0eb6d119f4e58b1d2bc
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-05-26 14:07:28 +0200

    climatology is based on the 1981-2019 only

--- bias_correction_second_step_final_bias_correction.py
+++ bias_correction_second_step_final_bias_correction.py
@@ -2,7 +2,7 @@
 import os
 
 # output directory (create it and go to it)
-output_dir = "/scratch/sutan101/forcing_for_beda_output_v2024-05-24_alternative/second_final_step/"
+output_dir = "/scratch/sutan101/forcing_for_beda_output_v2024-05-26_correct/second_final_step/"
 if os.path.exists(output_dir):
 	cmd = "rm -r " + output_dir
 	print(cmd); os.system(cmd)
@@ -12,17 +12,17 @@ os.chdir(output_dir)
 # input files
 
 # daily era5land in half degree resolution with the unit: m/day - AFTER THE FIRST STEP
-daily_era5land = "/scratch/sutan101/forcing_for_beda_output_v2024-05-24_alternative/first_step/merged_era5land_daily_original_1st_corrected_without_drizzle_1981-2022.nc"
+daily_era5land = "/scratch/sutan101/forcing_for_beda_output_v2024-05-26_correct/first_step/merged_era5land_daily_original_1st_corrected_without_drizzle_1981-2022.nc"
 
 # daily w5e5 in half degree resolution with the unit: m/day
 daily_w5e5     = "/scratch/sutan101/forcing_for_beda/w5e5/precipitation_daily_w5e5_1979-2019_mperday.nc"
 
 # calculate the climatology of daily_era5land and daily_w5e5
 monthly_climatology_era5land = "monthly_climatology_" + os.path.basename(daily_era5land)
-cmd = "cdo -L -f nc4 -setday,1 -setyear,2000 -ymonavg " + daily_era5land + " " + monthly_climatology_era5land
+cmd = "cdo -L -f nc4 -setday,1 -setyear,2000 -ymonavg -selyear,1981/2019 " + daily_era5land + " " + monthly_climatology_era5land
 print(cmd); os.system(cmd)
 monthly_climatology_w5e5 = "monthly_climatology_" + os.path.basename(daily_w5e5)
-cmd = "cdo -L -f nc4 -setday,1 -setyear,2000 -ymonavg " + daily_w5e5 + " " + monthly_climatology_w5e5 
+cmd = "cdo -L -f nc4 -setday,1 -setyear,2000 -ymonavg -selyear,1981/2019 " + daily_w5e5 + " " + monthly_climatology_w5e5 
 print(cmd); os.system(cmd)
 
 # defining the monthly_correction_factor: Pclim_W5E5 / Pclim_era5land
