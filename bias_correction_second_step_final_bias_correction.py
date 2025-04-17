ccb361866602666b8119ba6dc7701d44f6fdf09d
Edwin Sutanudjaja <edwinkost@UU105378>
2025-04-17 11:34:53 +0200

    start the second step of the correction

--- bias_correction_second_step_final_bias_correction.py
+++ bias_correction_second_step_final_bias_correction.py
@@ -2,7 +2,8 @@
 import os
 
 # output directory (create it and go to it)
-output_dir = "/scratch/sutan101/forcing_for_beda_output_v2024-05-26_correct/second_final_step/"
+# ~ output_dir = "/scratch/sutan101/forcing_for_beda_output_v2024-05-26_correct/second_final_step/"
+output_dir = "/scratch-shared/edwindan/era5land_corrected/second_step/"
 if os.path.exists(output_dir):
 	cmd = "rm -r " + output_dir
 	print(cmd); os.system(cmd)
@@ -12,10 +13,12 @@ os.chdir(output_dir)
 # input files
 
 # daily era5land in half degree resolution with the unit: m/day - AFTER THE FIRST STEP
-daily_era5land = "/scratch/sutan101/forcing_for_beda_output_v2024-05-26_correct/first_step/merged_era5land_daily_original_1st_corrected_without_drizzle_1981-2022.nc"
+# ~ daily_era5land = "/scratch/sutan101/forcing_for_beda_output_v2024-05-26_correct/first_step/merged_era5land_daily_original_1st_corrected_without_drizzle_1981-2022.nc"
+daily_era5land = "/scratch-shared/edwindan/era5land_corrected/first_step/merged/era5land_daily_original_1st_corrected_without_drizzle_1981-2024.nc"
 
 # daily w5e5 in half degree resolution with the unit: m/day
-daily_w5e5     = "/scratch/sutan101/forcing_for_beda/w5e5/precipitation_daily_w5e5_1979-2019_mperday.nc"
+# ~ daily_w5e5 = "/scratch/sutan101/forcing_for_beda/w5e5/precipitation_daily_w5e5_1979-2019_mperday.nc"
+daily_w5e5     = "/scratch-shared/edwin/era5land_wmo/w5e5/precipitation_daily_w5e5_1979-2019_mperday.nc"
 
 # calculate the climatology of daily_era5land and daily_w5e5
 monthly_climatology_era5land = "monthly_climatology_1981-2019_" + os.path.basename(daily_era5land)
@@ -30,9 +33,12 @@ cmd = "cdo -L -f nc4 -setrtoc,10,inf,10 -setmisstoc,1.0 -div " + monthly_climato
 print(cmd); os.system(cmd)
 
 
-for year in range(1981,2023):
+
+# ~ for year in range(1981,2023):
 
 # ~ for year in range(1981,1985):
+
+for year in range(1981,2025):
 
     
     # final step: # removing the bias, implementing the monthly_correction_factor
